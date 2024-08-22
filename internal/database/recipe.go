903f5e780c3ae8696f7a423c30bf276a71edebbe
BaDelhommer <badelhommer@gmail.com>
2024-08-22 17:34:33 -0500

    added recipe functionality

--- internal/database/recipe.go
+++ internal/database/recipe.go
@@ -7,3 +7,48 @@ type Recipe struct {
 	Temp        string   `json:"temp"`
 	Ingredients []string `json:"ingredients"`
 }
+
+func (db *DB) CreateRecipe(name, base, temp string, ingredients []string) (Recipe, error) {
+	dbStructure, err := db.LoadDB()
+	if err != nil {
+		return Recipe{}, err
+	}
+
+	id := len(dbStructure.Recipe) + 1
+	recipe := Recipe{
+		ID:          id,
+		Name:        name,
+		Base:        base,
+		Temp:        temp,
+		Ingredients: ingredients,
+	}
+
+	dbStructure.Recipe[id] = recipe
+	db.WriteDB(dbStructure)
+
+	return recipe, nil
+}
+
+func (db *DB) GetRecipe(id int) (Recipe, error) {
+	dbStructure, err := db.LoadDB()
+	if err != nil {
+		return Recipe{}, err
+	}
+
+	recipe, ok := dbStructure.Recipe[id]
+	if !ok {
+		return Recipe{}, err
+	}
+
+	return recipe, nil
+}
+
+func (db *DB) GetAllRecipes() (map[int]Recipe, error) {
+	dbStructure, err := db.LoadDB()
+	if err != nil {
+		return map[int]Recipe{}, err
+	}
+
+	return dbStructure.Recipe, nil
+
+}
